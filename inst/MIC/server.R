320834609b5ca9734f662ea416f513d7d342dab7
Iris Eekhout <iriseekhout@users.noreply.github.com>
2022-03-24 08:29:58 +0100

    update tool

--- inst/MIC/server.R
+++ inst/MIC/server.R
@@ -10,17 +10,247 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+
+
+    #set.seed (12345)
+
+    #start.time <- Sys.time()
+     vals <- reactiveValues(outputdf = data.frame(prop.imp = 0, mic.roc = 0, mic.pred = 0, mic.adj = 0, rel.trat = 0))
+    ## create variables that are to contain the outcomes of the simulations
+    observeEvent(input$goButton, {
+        shinyjs::show("showoutput")
+        shinyjs::hide("startscreen")
+#outputdf <- reactive({
+
+    mn.xo1 <- numeric(input$nt)           # mean observed PRO score at T1
+    sd.xo1 <- numeric(input$nt)           # SD observed PRO score at T1
+    mn.xo2 <- numeric(input$nt)           # mean observed PRO score at T2
+    sd.xo2 <- numeric(input$nt)           # SD observed PRO score at T2
+    dt <- data.frame(mn.xo1, sd.xo1, mn.xo2, sd.xo2)   # creates dataframe
+
+    dt$mn.xoc <- numeric(input$nt)        # mean observed change score
+    dt$sd.xoc <- numeric(input$nt)        # sd observed change score
+    dt$mn0.xoc <- numeric(input$nt)       # mean observed change score of not-improved
+    # subjects
+    dt$mn1.xoc <- numeric(input$nt)       # mean observed change score of improved
+    # subjects
+    dt$sd0.xoc <- numeric(input$nt)       # sd observed change score of not-improved
+    # subjects
+    dt$sd1.xoc <- numeric(input$nt)       # sd observed change score of improved
+    # subjects
+    dt$rel.xo1 <- numeric(input$nt)       # reliability observed PRO score T1
+    dt$rel.xo2 <- numeric(input$nt)       # reliability observed PRO score T2
+    dt$rel.xoc <- numeric(input$nt)       # reliability observed PRO change score
+    dt$rel.trat <- numeric(input$nt)      # reliability of transition ratings (TR)
+    dt$cor.trat.xoc <- numeric(input$nt)  # correlation between TR and observed PRO
+    # change score
+    dt$cor.xt1.xtc <- numeric(input$nt)   # correlation true PRO baseline and change
+    # score
+    dt$cor.xo1.xoc <- numeric(input$nt)   # correlation observed PRO baseline and
+    # change score
+    dt$prop.imp <- numeric(input$nt)      # proportion improved
+    dt$logodds  <- numeric(input$nt)      # log-odds of improvement
+    dt$true.mic <- numeric(input$nt)      # mean iMIC = genuine MIC
+    dt$sd.imic <- numeric(input$nt)       # sd of iMIC
+    dt$mic.roc <- numeric(input$nt)       # MIC(ROC-based)
+    dt$mic.pred <- numeric(input$nt)      # MIC(predictive)
+    dt$mic.adj <- numeric(input$nt)       # MIC(revised adjusted)
+    dt$residuals <- numeric(input$nt)     # genuine MIC minus MIC(revised adjusted)
+
+
+    ## Create starting point for the dataframe to be use in the simulations
+
+    xt1 <- numeric(input$ns)   # creates provisional true PRO measurement at T1
+    xt2 <- numeric(input$ns)   # creates provisional true PRO measurement at T2
+    xtc <- numeric(input$ns)   # creates provisional true change score variable
+    df <- data.frame(xt1, xt2, xtc)     # creates dataframe
+
+
+    ## Do the simulations
+
+    for(i in 1:input$nt)                  {
+
+        ## Simulate a dataset
+
+        # create "true" PRO measurement at T1
+        df$xt1 <- rnorm(n=input$ns, m=input$mn.true.t1, s=input$sd.true.t1)
+
+        ## Create a true change score that is correlated with the true T1 score
+        df$xtc <- input$cor.t1ch*(df$xt1 - mean(df$xt1)) +
+            sqrt(1-input$cor.t1ch^2)*rnorm(input$ns, 0, sd(df$xt1))
+        df$xtc <- df$xtc*input$sd.true.ch/sd(df$xtc) + input$mn.true.ch
+
+        mean(df$xtc)
+        sd(df$xtc)
+        cor(df$xt1, df$xtc)
+
+        # create "true" PRO measurement at T2
+        df$xt2 <- df$xt1 + df$xtc
+
+        # create observed PRO measurement at T1 by adding measurement error
+        sd.xt.error <- sqrt(((1-input$rel.t1)/input$rel.t1)*sd(df$xt1)^2)
+        df$xo1 <- round(df$xt1 + rnorm(n=input$ns, m=0, s=sd.xt.error))
+
+        # create observed PRO measurement at T2 by adding measurement error
+        df$xo2 <- round(df$xt2 + rnorm(n=input$ns, m=0, s=sd.xt.error))
+
+        # create observed PRO change score with measurement error
+        df$xoc <- df$xo2 - df$xo1
+
+        # create individual MIC values
+        df$imic <- rnorm(n=input$ns, m=input$mn.imic, s=input$sd.imic)
+
+        # create dichotomous transition ratings
+        sd.pc.error <- sqrt(((1-input$rel.trat)/input$rel.trat)*sd(df$xtc)^2)
+        df$xtc.percv <- df$xtc + rnorm(n=input$ns, m=0, s=sd.pc.error)
+
+        mean(df$xtc.percv)
+        sd(df$xtc.percv)
+        var(df$xtc)/var(df$xtc.percv)   # reliability of perceived change
+
+        df$trat <- 0
+        df$trat[df$xtc.percv > df$imic] <- 1
+        mean(df$trat)     # proportion improved
+
+        ## Do calculations and save values to variables
+
+        q <- mean(df$trat)              # q = proportion improved
+        dt$prop.imp[i]    <- q
+        dt$logodds[i]     <- log(q/(1-q))
+        dt$mn.xo1[i]      <- mean(df$xo1)
+        dt$mn.xo2[i]      <- mean(df$xo2)
+        dt$sd.xo1[i]      <- sd(df$xo1)
+        dt$sd.xo2[i]      <- sd(df$xo2)
+        dt$mn.xoc[i]      <- mean(df$xoc)
+        dt$sd.xoc[i]      <- sd(df$xoc)
+        dt$mn0.xoc[i]     <- mean(df[df$trat==0, ]$xoc)
+        dt$mn1.xoc[i]     <- mean(df[df$trat==1, ]$xoc)
+        dt$sd0.xoc[i]     <- sd(df[df$trat==0, ]$xoc)
+        dt$sd1.xoc[i]     <- sd(df[df$trat==1, ]$xoc)
+        dt$true.mic[i]    <- mean(df$imic)
+        dt$sd.imic[i]     <- sd(df$imic)
+        dt$cor.trat.xoc[i] <- cor(df$trat, df$xoc)
+        dt$cor.xt1.xtc[i] <- cor(df$xt1, df$xtc)
+        dt$cor.xo1.xoc[i] <- cor(df$xo1, df$xoc)
+        dt$rel.xo1[i]     <- var(df$xt1)/var(df$xo1)
+        dt$rel.xo2[i]     <- var(df$xt2)/var(df$xo2)
+        dt$rel.xoc[i]     <- var(df$xtc)/var(df$xoc)
+        dt$rel.trat[i]     <- var(df$xtc)/var(df$xtc.percv)
+
+
+
+
+        ### HEEL SIMPEL NU OPGEZET! HIER GEBLEVEN 22-10-21
+
+        ## Do ROC analysis
+
+        rocobj <- roc(df$trat, df$xoc, quiet = TRUE)
+
+        mic.roc <- coords(rocobj, x="best", input="threshold", ret="threshold",
+                          best.method="youden", transpose = TRUE)
+        dt$mic.roc[i] <- mic.roc[sample(length(mic.roc),1)]
+
+
+        ## Do logistic regression and calculate parameters and MIC(pred)
+
+        mylogit <- glm(trat ~ xoc, data = df, family = "binomial")
+
+        C <- coef(mylogit)[1]                 # intercept coefficient C
+        B <- coef(mylogit)[2]                 # regression coefficient B
+        p <- log(q/(1-q))                     # p = logodds(pre)
+
+        dt$mic.pred[i] <- (p-C)/B            # MIC(predicted)
+
+        ## "old" adjusted MIC
+        # cf <- 0.09 * dt$sd.xoc[i] + 0.103 * dt$sd.xoc[i] * dt$cor.trat.xoc[i]
+        # dt$mic.adj[i] <- dt$mic.pred[i] - cf * dt$logodds[i]
+
+        ## Revised adjusted MIC
+        rf <- (0.8/dt$rel.trat[i] - 0.5) * dt$sd.xoc[i] * dt$cor.trat.xoc[i]
+        ( dt$mic.adj[i] <- dt$mic.pred[i] - rf * p )
+
+        dt$residuals[i]   <- dt$true.mic[i] - dt$mic.adj[i]
+
+    }
+
+    vals$outputdf <- dt
+})
+
+
+
+    ###### RESULTS
+
+    # Mean proportion improve and 95% CI
+output$propimp <- renderTable({
+    data.frame(mean = round(mean(vals$outputdf$prop.imp),2),
+    t(round(quantile(vals$outputdf$prop.imp, c(0.025, 0.975)),2)))
+    })
+
+
+output$cortratoc <- renderTable({
+  data.frame(mean = round(mean(vals$outputdf$cor.trat.xoc),2),
+             t(round(quantile(vals$outputdf$cor.trat.xoc, c(0.025, 0.975)),2)))
+})
+
+output$micestimates <- renderTable({
+    # Mean ROC-based MIC and 95% CI
+   microc <- c(mean = mean(vals$outputdf$mic.roc),
+    quantile(vals$outputdf$mic.roc, c(0.025, 0.975)))
+
+  micpred <- c(
+    # Mean Predictive MIC and 95% CI
+    mean =round(mean(vals$outputdf$mic.pred),1),
+    round(quantile(vals$outputdf$mic.pred, c(0.025, 0.975)),1))
+  micadj <- c(
+    # Mean Adjusted MIC and 95% CI
+    mean = round(mean(vals$outputdf$mic.adj),1),
+    round(quantile(vals$outputdf$mic.adj, c(0.025, 0.975)),1))
+
+  rbind(microc,micpred, micadj)
+}, rownames = TRUE)
+
+
+output$trat <- renderTable(
+    data.frame(
+    # Mean TR reliability and 95% CI
+    mean = mean(vals$outputdf$rel.trat),
+    t(quantile(vals$outputdf$rel.trat, c(0.025, 0.975))))
+)
+
+    ##################################
+
+    ## If you want to see a summary of all variables of the samples:
+    ## Run the next line after removing the #
+    # summary(dt)
+
+    ## If you want to inspect all variables of the samples:
+    ## Run the next line after removing the #
+    # fix(dt)
+
+    ## If you want to save all variables of the samples:
+    ## Run the next line after removing the # (adjust the disk name "G:/")
+    # write.table(dt, file = "G:/sim.dat", sep = " ", row.names = F, col.names = T)
+
+
+
+
+
 
     output$distPlot <- renderPlot({
+        ggplot(data = vals$outputdf) +
+            geom_histogram(aes(mic.roc), fill = "blue", alpha = 0.25)+
+            geom_histogram(aes(mic.pred), fill = "green", alpha = 0.25)+
+            geom_histogram(aes(mic.adj), fill = "orange", alpha = 0.25)+
+            xlab("MIC estimates")
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        dtl <- vals$outputdf %>%
+            pivot_longer(cols = c(mic.roc, mic.pred, mic.adj), names_to = "method", values_to = "MIC")
 
+        ggplot(data = dtl) +
+            geom_histogram(aes(MIC, group = method, fill = method, color = method), alpha = 0.4)+
+            theme_light()
     })
 
 })
--- inst/MIC/ui.R
+++ inst/MIC/ui.R
@@ -8,26 +8,91 @@
 #
 
 library(shiny)
+library(shinyjs)
+library(dplyr)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("MIC validation tool"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            numericInput("ns",
+                         "Sample size",
+                         value = 200),
+            numericInput("nt",
+                         "Simulated samples",
+                         value = 1000),
+            numericInput("mn.true.t1",
+                         "Mean true baseline",
+                         value = 40),
+            numericInput("sd.true.t1",
+                         "SD true baseline",
+                         value = 10),
+            numericInput("rel.t1",
+                         "Reliability of baseline score",
+                         value = 0.8,
+                         min = 0,
+                         max = 1),
+            numericInput("mn.true.ch",
+                         "Mean true change score",
+                         value = 0),
+            numericInput("sd.true.ch",
+                         "SD true change score",
+                         value = 10),
+            numericInput("mn.imic",
+                         "Mean iMIC (= genuine MIC)",
+                         value = 5),
+            numericInput("sd.imic",
+                         "SD iMIC distribution",
+                         value = 0.5),
+            numericInput("cor.t1ch",
+                         "Correlation between true and change scores",
+                         value = -0.3,
+                         min = -1,
+                         max = 1),
+            numericInput("rel.trat",
+                         "Reliability of transition ratings",
+                         value = 0.4,
+                         min = 0,
+                         max = 1),
+            br(),
+            actionButton("goButton", "Go!")
+
+
+
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            shinyjs::useShinyjs(),
+            h1("Simulation tool for MIC estimation"),
+            p("Attachment to: Terluin B, Eekhout I, Terwee CB, Improved adjusted minimal important change takes reliability of transition ratings into account. J Clin Epidemiol 2022."),
+            p("With this tool you can replicate our simulations or perform simulations using your own simulation parameters. In the sidebar you can define your own parameters. The number of combinations of different parameters is infinite. You might want to try more extreme reliabilities or proportions improved and check how the adjusted MIC performs under these circumstances. Because you can simulate the genuine MIC, you can check whether the adjusted MIC is able to recover that MIC in terms of bias and precision. Note that you cannot directly simulate the proportion improved. The proportion improved depends on the values of the genuine MIC and the mean true change score. If the mean true change score is smaller than the genuine MIC the proportion improved will be <0.5; if the mean true change score is greater than the genuine MIC the proportion improved will be >0.5."),
+
+            tags$div(em("Note, for estimating an MIC in your own data, you can use the formula in the paper, or the R-package MIC.")),
+            br(),
+            div(id = "startscreen",
+                h3("Click GO! to start the simulations")
+                ),
+            div(id = "showoutput",
+            h3("Proportion improved"),
+            p("Mean of simulated samples and the 2.5th and 97.5th quantiles"),
+            tableOutput("propimp"),
+            h3("Reliability of transition ratings (TR)"),
+            p("Mean of simulated samples and the 2.5th and 97.5th quantiles"),
+            tableOutput("trat"),
+            h3("Point biserial correlation between the transition ratings (TR) and the change score"),
+            p("Mean of simulated samples and the 2.5th and 97.5th quantiles"),
+            tableOutput("cortratoc"),
+            h3("Estimated MIC values"),
+            plotOutput("distPlot"),
+            tableOutput("micestimates")
+            )%>% shinyjs::hidden()
         )
     )
 ))
